8f7fd07dfbdd5883024f356f6741cd3286a0256a
Cody Jackson <cajackson05@icloud.com>
2024-06-19 18:57:07 +1000

    Creating addEmployee function functionality

--- EmployeePortal/Source.cpp
+++ EmployeePortal/Source.cpp
@@ -5,6 +5,9 @@
 #include <memory>
 #include <stdexcept>
 #include <map>
+#include <regex>
+
+#include <typeinfo> // remove
 
 using namespace std;
 
@@ -39,6 +42,7 @@ private:
     Time schedule[7][2]; // 7 Days per Week | [Clock In Time, Clock Out Time]
     vector<string> alerts;
 public:
+    virtual ~User() = default; // Makes the user class a polymorphic type to allow for dynamic_cast
     User(int id, string user, string pass, bool perm, int Pay, string sched) : ID(id), username(user), password(pass), permanent(perm), pay(Pay) {
         // split sched string into the schedule array
         string tempHour, tempMinute;
@@ -75,7 +79,7 @@ public:
     bool login(string user, string pass) const {
         return user == username && pass == password;
     }
-    void viewSchedule() const {
+    void viewSchedule() {
         map<int, string> weekDays{
             {0, "Monday" },
             {1, "Tuesday" },
@@ -171,6 +175,7 @@ public:
         try {
             shared_ptr<User> user = findUserByID(ID, users);
             if (!user->isPermanent()) {
+                user->viewSchedule(); // Display schedule
                 // Change schedule
             }
             else {
@@ -185,7 +190,7 @@ public:
     void findSchedule(int ID, vector<shared_ptr<User>>& users) {
         try {
             shared_ptr<User> user = findUserByID(ID, users);
-            // Display schedule
+            user->viewSchedule(); // Display schedule
         }
         catch (invalidUser& e) {
             cout << "Error: " << e.what() << endl;
@@ -202,6 +207,8 @@ public:
     void changeSchedule(int ID, vector<shared_ptr<User>>& users) override {
         try {
             shared_ptr<User> user = findUserByID(ID, users);
+
+            user->viewSchedule(); // Display schedule
             // Change schedule
         }
         catch (invalidUser& e) {
@@ -210,7 +217,109 @@ public:
         }
     }
     void addEmployee(vector<shared_ptr<User>>& users) {
-        // Add functionality
+        // Get ID
+        int ID;
+        cout << "Enter the desired ID for the user: ";
+        while (!(cin >> ID) || ID <= 0) {
+            cout << "Invalid input. Please enter a positive integer for the ID: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        // Check if ID taken
+        for (auto& user : users) {
+            if (user->getID() == ID) {
+                cout << "This ID is already taken!\n";
+                return;
+            }
+        }
+
+        // Get Name
+        string user;
+        cout << "Enter the name of this user: ";
+        cin.ignore();
+        getline(cin, user);
+
+        // Get Pass
+        string pass;
+        cout << "Enter the pass of this user: ";
+        getline(cin, pass);
+
+        // Get permenant work status
+        char tempVal;
+        bool perm;
+        cout << "Is this user a permanent or casual employee (P/C): ";
+        cin >> tempVal;
+        tempVal = toupper(tempVal);
+        while (tempVal != 'P' && tempVal != 'C') {
+            cout << "Invalid input. Please enter 'P' for permanent or 'C' for casual: ";
+            cin >> tempVal;
+            tempVal = toupper(tempVal);
+        }
+        perm = (tempVal == 'P');
+        
+        // Get Pay
+        int payRate;
+        cout << "What " << ((perm) ? "yearly" : "hourly") << " salary is this user on: ";
+        while (!(cin >> payRate) || payRate <= 0) {
+            cout << "Invalid input. Please enter a positive integer for the pay rate: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        // Get schedule
+        string schedule = "";
+        map<int, string> weekDays{
+            {0, "Monday" },
+            {1, "Tuesday" },
+            {2, "Wednesday" },
+            {3, "Thursday" },
+            {4, "Friday" },
+            {5, "Saturday" },
+            {6, "Sunday" }
+        };
+        for (int i = 0; i < 7; i++) {
+            string temp;
+            cout << "Enter the start and end time in this format for the day " << weekDays[i] << "\nhour:minute hour:minute";
+            cin >> temp;
+            // Validation to ensure correct format
+            regex timePattern(R"((\d{1,2}:\d{2}) (\d{1,2}:\d{2}))");
+            while (!regex_match(temp, timePattern)) {
+                cout << "Invalid format. Please enter the time in 'hour:minute hour:minute' format: ";
+                getline(cin, temp);
+            }
+            schedule += temp + " ";
+        }
+        
+        // Get what type of employee this user is
+        // Push_back shared ptr to users vector
+        cout << "Is this user an Employee, Accountant, Duty Manager, or Manager (E/A/D/M): ";
+        cin >> tempVal;
+        tempVal = toupper(tempVal);
+        while (tempVal != 'E' && tempVal != 'A' && tempVal != 'D' && tempVal != 'M') {
+            cout << "Invalid input. Please enter 'E' for Employee, 'A' for Accountant, 'D' for Duty Manager, or 'M' for Manager: ";
+            cin >> tempVal;
+            tempVal = toupper(tempVal);
+        }
+
+        switch (tempVal) {
+        case 'E': {
+            users.push_back(make_shared<Employee>(ID, user, pass, perm, payRate, schedule));
+            break;
+        }
+        case 'A': {
+            users.push_back(make_shared<Accountant>(ID, user, pass, perm, payRate, schedule));
+            break;
+        }
+        case 'D': {
+            users.push_back(make_shared<DutyManager>(ID, user, pass, perm, payRate, schedule));
+            break;
+        }
+        case 'M': {
+            users.push_back(make_shared<Manager>(ID, user, pass, perm, payRate, schedule));
+            break;
+        }
+        }
     }
 };
 
@@ -226,13 +335,15 @@ void saveToFile(vector<shared_ptr<User>> users) {
 
 int main() {
     vector<shared_ptr<User>> users = loadFromFile();
-    shared_ptr<User> currentUser = nullptr;
 
     // Examples
     users.push_back(make_shared<Employee>(1, "emp_user", "password", false, 70000, "9:00 17:00 9:00 17:00 0:00 0:00 0:00 0:00 0:00 0:00 17:00 1:00 17:00 1:00"));
     users.push_back(make_shared<Accountant>(2, "acc_user", "password", true, 80000, "9:00 17:00 9:00 17:00 9:00 17:00 9:00 17:00 9:00 17:00 0:00 0:00 0:00 0:00"));
     users.push_back(make_shared<DutyManager>(3, "dm_user", "password", false, 90000, "10:00 18:00 10:00 18:00 10:00 18:00 10:00 18:00 10:00 18:00 0:00 0:00 0:00 0:00"));
     users.push_back(make_shared<Manager>(4, "mgr_user", "password", true, 100000, "8:00 16:00 8:00 16:00 8:00 16:00 8:00 16:00 8:00 16:00 0:00 0:00 0:00 0:00"));
+
+    auto currentUser = dynamic_pointer_cast<Manager>(users[3]); // Manager
+    currentUser->addEmployee(users);
 
     for (auto& user : users) {
         cout << user->getName() << endl; 
